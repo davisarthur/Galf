6cb48df311338ed0598ab308e4e3221625a258f8
Davis Arthur <dsarthur123@gmail.com>
2020-01-05 23:44:21 -0600

    update

--- Galf/GameScene.swift
+++ Galf/GameScene.swift
@@ -27,7 +27,7 @@ class GameScene: SKScene {
         self.tileMap = self.childNode(withName: "//TileMap") as! SKTileMapNode?
         self.switchButton = self.childNode(withName: "//SwitchButton") as! SKSpriteNode?
         
-        createTerrainPoints()
+//        createTerrainPoints()
         
         // Create ground
         let ground = SKShapeNode(splinePoints: &points, count: points.count)
@@ -122,112 +122,112 @@ class GameScene: SKScene {
         return (buttonRect?.contains(touch))!
     }
     
-    func createTerrainPoints() {
-        // Create tile map
-        for col in 0...tileMap!.numberOfColumns - 1 {
-            for ro in stride(from: tileMap!.numberOfRows - 1, to: 0, by: -1) {
-                var def = tileMap?.tileDefinition(atColumn: col, row: ro)?.name
-                if def != nil {
-                    var incomplete = false
-                    var usingBuffer = false
-                    var newPoints : [CGPoint] = []
-                    var bufferPoints : [CGPoint] = []
-                    var ro1 = ro
-                    var xBefore = CGFloat(0.0)
-                    if points.count > 0 {
-                        xBefore = points[points.count - 1].x
-                    }
-                    else {
-                        xBefore = ((tileMap?.centerOfTile(atColumn: col, row: ro).x)! - 16.0)
-                    }
-                    while true {
-                        if def == "Top" {
-                            newPoints = TerrainBuilder.createTop(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making Top")
-                            }
-                        else if def == "TopRight" {
-                            newPoints = TerrainBuilder.createTopRight(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making TopRight")
-                            }
-                        else if def == "BotLeftSlant" {
-                            newPoints = TerrainBuilder.createBotLeftSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making BotLeftSlant")
-                            }
-                        else if def == "LeftCurved" {
-                            newPoints = TerrainBuilder.createLeftCurved(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making LeftCurved")
-                        }
-                        else if def == "TopLeftSlant" {
-                            newPoints = TerrainBuilder.createTopLeftSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making TopLeftSlant")
-                        }
-                        else if def == "TopLeft" {
-                            newPoints = TerrainBuilder.createTopLeft(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making TopLeft")
-                        }
-                        else if def == "RightCurved" {
-                            newPoints = TerrainBuilder.createRightCurved(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making RightCurved")
-                        }
-                        else if def == "TopRightSlant" {
-                            newPoints = TerrainBuilder.createTopRightSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making TopRightSlant")
-                        }
-                        else if def == "BotRightSlant" {
-                            newPoints = TerrainBuilder.createBotRightSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
-                            print("Making BotRightSlant")
-                        }
-                        else {
-                            print("Atypical terrain tile. Definition: \(String(describing: def))")
-                        }
-                        
-                        print(newPoints)
-                        
-                        // If the points array is empty, the first tile must be added.
-                        if points.count == 0 {
-                            points += Array(newPoints[0...newPoints.count - 1])
-                            print("added newPoints")
-                            }
-                        // Otherwise...
-                        else {
-                            // If the leftmost point in newPoints matches the rightmost point in points, append newPoints
-                            if points[points.count - 1] == newPoints[0] {
-                                points += Array(newPoints[1...newPoints.count - 1])
-                                print("added newPoints")
-                                // Append bufferPoints after appending newPoints
-                                if bufferPoints.count > 0 {
-                                    // provided bufferPoints and points are continuous
-                                    if points[points.count - 1] == bufferPoints[0] {
-                                        points += Array(bufferPoints[1...bufferPoints.count - 1])
-                                        bufferPoints = []
-                                        }
-                                    else {
-                                        print("Invalid buffer")
-                                        }
-                                    break
-                                    }
-                                }
-                            else {
-                                bufferPoints = newPoints
-                                print("placing in buffer")
-                                usingBuffer = true
-                            }
-                        }
-                        incomplete = !usingBuffer && points[points.count - 1].x - xBefore != 32.0
-                        
-                        if incomplete || usingBuffer {
-                            ro1 -= 1
-                            def = self.tileMap?.tileDefinition(atColumn: col, row: ro1)?.name
-                            continue
-                            }
-                        else {
-                            break
-                        }
-                    }
-                    break
-                }
-            }
-        }
-    }
+//    func createTerrainPoints() {
+//        // Create tile map
+//        for col in 0...tileMap!.numberOfColumns - 1 {
+//            for ro in stride(from: tileMap!.numberOfRows - 1, to: 0, by: -1) {
+//                var def = tileMap?.tileDefinition(atColumn: col, row: ro)?.name
+//                if def != nil {
+//                    var incomplete = false
+//                    var usingBuffer = false
+//                    var newPoints : [CGPoint] = []
+//                    var bufferPoints : [CGPoint] = []
+//                    var ro1 = ro
+//                    var xBefore = CGFloat(0.0)
+//                    if points.count > 0 {
+//                        xBefore = points[points.count - 1].x
+//                    }
+//                    else {
+//                        xBefore = ((tileMap?.centerOfTile(atColumn: col, row: ro).x)! - 16.0)
+//                    }
+//                    while true {
+//                        if def == "Top" {
+//                            newPoints = TerrainBuilder.createTop(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making Top")
+//                            }
+//                        else if def == "TopRight" {
+//                            newPoints = TerrainBuilder.createTopRight(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making TopRight")
+//                            }
+//                        else if def == "BotLeftSlant" {
+//                            newPoints = TerrainBuilder.createBotLeftSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making BotLeftSlant")
+//                            }
+//                        else if def == "LeftCurved" {
+//                            newPoints = TerrainBuilder.createLeftCurved(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making LeftCurved")
+//                        }
+//                        else if def == "TopLeftSlant" {
+//                            newPoints = TerrainBuilder.createTopLeftSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making TopLeftSlant")
+//                        }
+//                        else if def == "TopLeft" {
+//                            newPoints = TerrainBuilder.createTopLeft(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making TopLeft")
+//                        }
+//                        else if def == "RightCurved" {
+//                            newPoints = TerrainBuilder.createRightCurved(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making RightCurved")
+//                        }
+//                        else if def == "TopRightSlant" {
+//                            newPoints = TerrainBuilder.createTopRightSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making TopRightSlant")
+//                        }
+//                        else if def == "BotRightSlant" {
+//                            newPoints = TerrainBuilder.createBotRightSlant(center: (tileMap?.centerOfTile(atColumn: col, row: ro1))!)
+//                            print("Making BotRightSlant")
+//                        }
+//                        else {
+//                            print("Atypical terrain tile. Definition: \(String(describing: def))")
+//                        }
+//
+//                        print(newPoints)
+//
+//                        // If the points array is empty, the first tile must be added.
+//                        if points.count == 0 {
+//                            points += Array(newPoints[0...newPoints.count - 1])
+//                            print("added newPoints")
+//                            }
+//                        // Otherwise...
+//                        else {
+//                            // If the leftmost point in newPoints matches the rightmost point in points, append newPoints
+//                            if points[points.count - 1] == newPoints[0] {
+//                                points += Array(newPoints[1...newPoints.count - 1])
+//                                print("added newPoints")
+//                                // Append bufferPoints after appending newPoints
+//                                if bufferPoints.count > 0 {
+//                                    // provided bufferPoints and points are continuous
+//                                    if points[points.count - 1] == bufferPoints[0] {
+//                                        points += Array(bufferPoints[1...bufferPoints.count - 1])
+//                                        bufferPoints = []
+//                                        }
+//                                    else {
+//                                        print("Invalid buffer")
+//                                        }
+//                                    break
+//                                    }
+//                                }
+//                            else {
+//                                bufferPoints = newPoints
+//                                print("placing in buffer")
+//                                usingBuffer = true
+//                            }
+//                        }
+//                        incomplete = !usingBuffer && points[points.count - 1].x - xBefore != 32.0
+//
+//                        if incomplete || usingBuffer {
+//                            ro1 -= 1
+//                            def = self.tileMap?.tileDefinition(atColumn: col, row: ro1)?.name
+//                            continue
+//                            }
+//                        else {
+//                            break
+//                        }
+//                    }
+//                    break
+//                }
+//            }
+//        }
+//    }
 
 }
--- Galf/TerrainBuilder.swift
+++ Galf/TerrainBuilder.swift
@@ -12,6 +12,95 @@ import GameplayKit
 
 struct TerrainBuilder {
     
+//    static func createTop(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x - 16.0, y: center.y)
+//        let right = CGPoint(x: center.x + 16.0, y: center.y)
+//        output.append(left)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createTopRight(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x - 16.0, y: center.y)
+//        let middle = CGPoint(x: center.x - 8.0, y: center.y - 4.0)
+//        let right = CGPoint(x: center.x, y: center.y - 16.0)
+//        output.append(left)
+//        output.append(middle)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createTopLeftSlant(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x, y: center.y + 16.0)
+//        let right = CGPoint(x: center.x + 16.0, y: center.y - 16.0)
+//        output.append(left)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createLeftCurved(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x, y: center.y + 16.0)
+//        let middle = CGPoint(x: center.x + 8.0, y: center.y + 4.0)
+//        let right = CGPoint(x: center.x + 16.0, y: center.y)
+//        output.append(left)
+//        output.append(middle)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createBotLeftSlant(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x - 16.0, y: center.y + 16.0)
+//        let right = CGPoint(x: center.x, y: center.y - 16.0)
+//        output.append(left)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createTopLeft(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x, y: center.y - 16.0)
+//        let middle = CGPoint(x: center.x + 8.0, y: center.y - 4.0)
+//        let right = CGPoint(x: center.x + 16.0, y: center.y)
+//        output.append(left)
+//        output.append(middle)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createRightCurved(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x - 16.0, y: center.y)
+//        let middle = CGPoint(x: center.x - 8.0, y: center.y + 4.0)
+//        let right = CGPoint(x: center.x, y: center.y + 16.0)
+//        output.append(left)
+//        output.append(middle)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createTopRightSlant(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x - 16.0, y: center.y - 16.0)
+//        let right = CGPoint(x: center.x, y: center.y + 16.0)
+//        output.append(left)
+//        output.append(right)
+//        return output
+//    }
+//
+//    static func createBotRightSlant(center: CGPoint) -> [CGPoint] {
+//        var output : [CGPoint] = []
+//        let left = CGPoint(x: center.x, y: center.y - 16.0)
+//        let right = CGPoint(x: center.x + 16.0, y: center.y + 16.0)
+//        output.append(left)
+//        output.append(right)
+//        return output
+//    }
+    
     static func createTop(center: CGPoint) -> [CGPoint] {
         var output : [CGPoint] = []
         let left = CGPoint(x: center.x - 16.0, y: center.y)
